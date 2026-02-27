f10bc707269b387fab0a72e89fe1312082e41559
Facultad De Sistemas <sistemas@academico.unab.ad>
2026-02-27 17:50:09 -0500

    chore: Pantalla 2 ahi va

--- app/src/main/java/com/alejandromoreno/quizandroid/MainActivity.kt
+++ app/src/main/java/com/alejandromoreno/quizandroid/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -11,14 +12,21 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 
 class MainActivity : ComponentActivity() {
@@ -27,6 +35,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Pantalla()
+            PantallaDos()
 
 
                 }
@@ -82,4 +91,34 @@ fun Pantalla(){
                 .padding(all = 10.dp)
             )
     }
+}
+@Preview(showSystemUi = true)
+@Composable
+fun PantallaDos(){
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .fillMaxWidth()
+            .padding(all = 30.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        val painter = painterResource(R.drawable.gus)
+        Image(
+            painter = painter,
+            contentDescription = "Gustavo Fring",
+            modifier = Modifier
+                .size(100.dp)
+                .clip(CircleShape),
+            contentScale = ContentScale.Crop
+        )
+        Text(
+            text = "Gustavo Fring",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold
+        )
+        Text(modifier = Modifier,
+            text = "Empresario y magnate, dueño de múltiples franquicias"
+        )
+    }
+
 }
